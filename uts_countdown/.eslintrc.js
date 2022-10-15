9f9266e7418e6d40ec885757651d1bf6ea6eb2f0
neszha <fanesahadi@gmail.com>
2022-10-15 21:40:54 +0700

    Setup eslint

---
+++ uts_countdown/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        // "airbnb"
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'react/prop-types': 'off',
+        'class-methods-use-this': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-use-before-define': ['error', { variables: false }],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    },
+};
