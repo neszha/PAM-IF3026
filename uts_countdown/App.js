046a5e21a912b0d96c1a5a121227b9c411dd6dba
neszha <fanesahadi@gmail.com>
2022-10-16 21:24:22 +0700

    after builder test

--- uts_countdown/App.js
+++ uts_countdown/App.js
@@ -1,9 +1,9 @@
 import { Component } from 'react';
-// import * as Font from 'expo-font';
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-// import Navigator from './src/Navigator.jsx';
-// import { countdownHelper } from './src/helper/index.js';
+import * as Font from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Navigator from './src/Navigator.jsx';
+import { countdownHelper } from './src/helper/index.js';
 
 class App extends Component {
     state = {
@@ -15,30 +15,21 @@ class App extends Component {
     }
 
     async _loadFonts() {
-        // const fonts = {
-        //     'Urbanist-Black': require('./src/assets/fonts/urbanist/Urbanist-Black.ttf'),
-        //     'Urbanist-Regular': require('./src/assets/fonts/urbanist/Urbanist-Regular.ttf'),
-        // };
-        // await Font.loadAsync(fonts);
+        const fonts = {
+            'Urbanist-Black': require('./src/assets/fonts/urbanist/Urbanist-Black.ttf'),
+            'Urbanist-Regular': require('./src/assets/fonts/urbanist/Urbanist-Regular.ttf'),
+        };
+        await Font.loadAsync(fonts);
         this.setState({ fontLoaded: true });
-        // countdownHelper.runtime();
+        countdownHelper.runtime();
     }
 
     render() {
         if (!this.state.fontLoaded) return null;
         return (
             <View style={st.container}>
-                {/* <StatusBar style="auto" /> */}
-                {/* <Navigator /> */}
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
-                <Text>Haiii</Text>
+                <StatusBar style="auto" />
+                <Navigator />
             </View>
         );
     }
